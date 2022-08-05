fb0bbffaa82515531b553b421b148551249ee837
Grand-breakeer <110274520+Grand-breakeer@users.noreply.github.com>
2022-08-05 12:14:28 +0800

    Add files via upload

---
+++ 初识常量和宏.c
@@ -0,0 +1,37 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//
+//
+//
+//
+//
+//
+////define预处理指令
+////1.定义符号
+//
+//
+////#define MAX 1000//#define定义常量
+////
+////int main()
+////{
+////
+////
+////	printf("%d\n",MAX );
+////	return 0;
+////}
+//
+//
+//
+////#define定义宏
+////#define ADD(x,y) x+y//#define定义的宏是替换，将ADD(X,Y)替换为x+y
+//#define ADD(X,Y) ((X)+(Y))
+//int main()
+//{
+//
+//
+//	printf("%d\n", 4*ADD(3,4));
+//
+//
+//	return 0;
+//}
