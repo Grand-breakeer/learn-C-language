24855492a80a4e4998f832921dc2ebdc00ecec2a
Grand-breakeer <110274520+Grand-breakeer@users.noreply.github.com>
2022-08-06 17:59:43 +0800

    Add files via upload

---
+++ 初识结构体.c
@@ -0,0 +1,46 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//
+//
+////结构体可以让C语言创造新的类型
+//
+////例如创建学生
+//struct stu
+//{
+//	char name[20];//成员变量
+//	int age;
+//	double score;
+//};
+//
+////创建一个书的类型
+//struct Book
+//{
+//	char name[20];
+//	float price;
+//	char id[30];
+//};
+//
+//
+//int main()
+//{
+//	struct stu s = { "mike",20,52.2 };//结构体的创建和初始化
+//	printf("1: %s %d %lf\n", s.name, s.age, s.score);//结构体.成员变量可以找到对应数据
+//	
+//	
+//	struct stu * ps = &s;
+//	printf("2: %s %d %lf\n", (*ps).name, (*ps).age, (*ps).score);
+//
+//	printf("3: %s %d %lf\n", ps->name, ps->age, ps->score);//结构体->成员变量
+//
+//
+//	return 0;
+//}
+//
+//
+//
+//
+//
+//
+//
+
