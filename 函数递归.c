301bba8f5650e011bea1d48c32eda0bdc9fc28eb
Grand-breakeer <110274520+Grand-breakeer@users.noreply.github.com>
2022-08-14 18:32:11 +0800

    Add files via upload

--- 函数递归.c
+++ 函数递归.c
@@ -1,10 +1,10 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
-#include<string.h>
-#include<windows.h>
-#include<time.h>
-#include<stdlib.h>
-#include<math.h>
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#include<string.h>
+//#include<windows.h>
+//#include<time.h>
+//#include<stdlib.h>
+//#include<math.h>
 //函数自己调用自己叫递归
 // 1.递归存在限制条件，满足条件便不再继续
 // 2.每次递归后越来越接近限制条件
@@ -89,3 +89,238 @@
 //	return 0;
 //}
 
+//递归和迭代
+//求n的阶乘
+//递归
+//int s(int n)
+//{
+//	if (n > 1)
+//	{
+//		return n*s(n - 1);
+//	}
+//	else
+//		return n;
+//}
+//int main()
+//{
+//	int a = 0;
+//	
+//	scanf("%d", &a);
+//	printf("%d", s(a));
+//	return 0;
+//}
+//迭代
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 1;
+//	scanf("%d", &a);
+//	for (b = 1; b <= a; b++)
+//	{
+//		c = c * b;
+//	}
+//	printf("%d", c);
+//	return 0;
+//}
+
+//求第n个斐波那契数
+//int count = 0;
+//int s(n)//效率太低进行了重复大量的计算
+//{
+//	//统计第三个斐波那契数的计算次数
+//	if (n == 3)
+//	{
+//		count++;
+//	}
+//	if (n > 2)
+//	{
+//		return s(n-1)+s(n-2);
+//	}
+//	else
+//		return 1;
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	printf("%d", s(a));
+//	printf("%d", count);
+//	return 0;
+//}
+//循环求解效率高
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 1;
+//	int d = 1;
+//	int e = 0;
+//	scanf("%d", &a);
+//	if (a > 2)
+//	{
+//		for (b = 3; b <= a; b++)
+//		{
+//			e = c;
+//			c = c + d;
+//			d = e;	
+//		}
+//		printf("%d", c);
+//	}
+//	else
+//	{
+//		printf("%d", 1);
+//	}
+//	return 0;
+//}
+
+//汉罗塔问题
+//int s(int n)
+//{
+//	while (n >= 1)
+//	{
+//		n = n - 1;
+//		return 2 * s(n) + 1;
+//		
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	printf("%d", s(a));
+//	return 0;
+//}
+
+//青蛙跳台
+//int p(int n)
+//{
+//	if (n > 1)
+//	{
+//		return p(n - 1) + p(n - 2);
+//	}
+//	return 1;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	printf("%d", p(a));
+//	return 0;
+//}
+
+
+//1到100中出现了多少个数字9
+//int main()
+//{
+//	int a = 0;
+//	int count = 0;
+//	for (a = 1; a <= 100; a++)
+//	{
+//		if (a/10==9)
+//		{
+//			count++;
+//		}
+//		if (a % 10 == 9)
+//		{
+//			count++;
+//		}
+//	}
+//	printf("%d", count);
+//	return 0;
+//}
+
+
+//计算1/1-1/2+1/3-1/4........+1/99-1/100
+//int main()
+//{
+//	int i = 0;
+//	double sum = 0.0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 2 == 0)
+//		{
+//			sum -= 1.0 / i;
+//		}
+//		else
+//		{
+//			sum += 1.0 / i;
+//		}
+//	}
+//	printf("%f", sum);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	double sum = 0.0;
+//	int flag = 1;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		sum += flag * 1.0 / i;
+//		flag=- flag;
+//	}
+//	printf("%lf\n", sum);
+//	return 0;
+//}
+
+//求十个整数最大值
+//int main()
+//{
+//	int arr[] = { -15,-2,-3,-4,-5,-6,-7,-8,-9,-10 };
+//	int max = arr[1];//直接从数组中挑一个元素
+//	int i = 0;
+//	for (i = 0; i <= 9; i++)
+//	{
+//		if (arr[i] > max)
+//		{
+//			max = arr[i];
+//		}
+//	}
+//	printf("%d", max);
+//	return 0;
+//}
+
+//输出乘法口诀表
+//int main()
+//{
+//	int i = 1;
+//	//打印行数
+//	for (i = 1; i <= 9; i++)
+//	{
+//		//打印一行
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%-2d  ", i, j, i * j);//2d打印两位-为左对齐
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+//逗号表达式就是从左到右最右边的袁术
+
+//int main()
+//{
+//	int i = 0;
+//	int a = 0;
+//	scanf("%d", &a);
+//	for (i = 1; i <= a; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//
+//		{
+//			printf("%d*%d=%-6d", i, j, i * j);
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
