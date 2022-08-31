356d676f241a47f9c43a31360b8f5ee81a0694ef
Grand-breakeer <110274520+Grand-breakeer@users.noreply.github.com>
2022-08-31 12:56:09 +0800

    Add files via upload

--- 初阶调试技巧/实用调试技巧.c
+++ 初阶调试技巧/实用调试技巧.c
@@ -1,4 +1,4 @@
-#define _crt_secure_no_warnings 1
+#define  _CRT_SECURE_NO_WARNINGS	
 #include <stdio.h>
 #include<string.h>
 #include<windows.h>
@@ -12,33 +12,33 @@
 //release发布版本文件体量小
 
 
-void test()
-{
-	int a = 10;
-	int b = 2;
-	int c = a + b;
-}
-
-
-int main()
-{
-	int arr[10] = { 0 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-
-	int i = 0;
-	for (i = 0; i < sz; i++)
-	{
-		arr[i] = i + 1;
-	}
-	for (i = 0; i < sz; i++)
-	{
-		test();
-		printf("%d\n", arr[i]);
-	}
-
-
-	return 0;
-}
+//void test()
+//{
+//	int a = 10;
+//	int b = 2;
+//	int c = a + b;
+//}
+//
+//
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		arr[i] = i + 1;
+//	}
+//	for (i = 0; i < sz; i++)
+//	{
+//		test();
+//		printf("%d\n", arr[i]);
+//	}
+//
+//
+//	return 0;
+//}
 
 //环境准备
 
@@ -58,3 +58,117 @@ int main()
 
 
 //调试程序时查询相关信息
+
+
+//函数调用堆栈反应的是函数调用逻辑（入栈--出栈）
+//void test2()
+//{
+//	printf("hehe\n");
+//}
+//void test1()
+//{
+//	test2();
+//}
+//void test()
+//{
+//	test1();
+//}
+//
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+
+
+//void test()
+//{
+//	int a = 10;
+//	int b = 2;
+//	int c = a + b;
+//}
+//
+//
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		arr[i] = i + 1;
+//	}
+//	for (i = 0; i < sz; i++)
+//	{
+//		test();
+//		printf("%d\n", arr[i]);
+//	}
+//
+//
+//	return 0;
+//}
+
+//案例1
+//调试解决的是运行错误并不是语法错误
+
+
+//对每一步代码都有预期值，再与实际值比较才能更好地找到问题
+
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;
+//	int n = 0;
+//	//int ret = 1;//应该在j循环后重置ret的值
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;
+//		int ret = 1;
+//		for (j = 1; j <= i; j++)
+//		{
+//			ret *= j;
+//		}
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 0;
+//	int ret = 1;
+//	int sum = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret *= i;
+//		sum += ret;
+//	}
+//	printf("%d", sum);
+//	return 0;
+//}
+
+
+//1.i和arr是局部变量局部变量是放在栈区上的
+
+//栈区地址使用习惯是先使用高地址空间再使用低地址空间
+
+//2.数组随着下标增长地址由低到高变化的
+
+//int main()
+//{
+//	
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	for (i = 0; i <= 19; i++)
+//	{
+//		arr[i] = 0;
+//		printf("hehe\n");
+//	}
+//	//代码运行结果--死循环
+//	return 0;
+//}
