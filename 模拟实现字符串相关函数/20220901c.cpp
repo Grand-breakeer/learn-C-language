45b7884239db5e581884ae268e9ac7b2be6d44ac
Grand-breakeer <110274520+Grand-breakeer@users.noreply.github.com>
2022-09-01 22:55:04 +0800

    Add files via upload

--- 模拟实现字符串相关函数/20220901c.cpp
+++ 模拟实现字符串相关函数/20220901c.cpp
@@ -9,27 +9,52 @@
 
 
 //模拟实现strcpy
-//void my_strcpy(char* dest,const char* src)
-//{
-//	assert(src != NULL);//断言（此处防止传入空指针）可以报错并提供参考信息协助解除bug
-//	assert(dest != NULL);
-//	while (*dest++ = *src++)
-//	{
-//		//hello的拷贝
-//	}
-//	//\0的拷贝
-//}
-//
-//
-//int main()
-//{
-//	char arr1[20] = "xxxxxxxxxxxxxxx";
-//	char arr2[] = "hello";
-//	my_strcpy(arr1, arr2);//1.目标空间的起始地址，2.源空间的起始地址
-//	printf("%s\n", arr1);
-//
-//	return 0;
-//}
+//把src指向的内容拷贝放进dest指向的空间中
+//从本质上讲，希望dest指向的内容被修改，src指向的内容不可被修改
+//strcpy其实返回的是目标空间的起始地址
+
+
+char* my_strcpy(char* dest,const char* src)
+{
+	assert(src != NULL);//断言（此处防止传入空指针）可以报错并提供参考信息协助解除bug
+	assert(dest != NULL);
+	char* ret = dest;
+	while (*dest++ = *src++)
+	{
+		;//hello的拷贝
+	}
+	//\0的拷贝
+	return ret;
+
+}//标准库函数的模拟实现
+
+
+//代码的健壮性（鲁棒性）降低容错率保护代码
+
+
+//模拟实现一个strlen求字符串长度
+size_t my_strlen(const char* str)//size_t本质是size_t
+{
+	assert(str != NULL);
+	size_t len = 0;
+	while (*str != '\0')
+	{
+		str++;
+		len++;
+	}
+	return len;
+}
+
+int main()
+{
+	char arr1[20] = "xxxxxxxxxxxxxxx";
+	char arr2[] = "hello";
+	//1.目标空间的起始地址，2.源空间的起始地址
+	printf("%s\n", my_strcpy(arr1, arr2));
+	printf("%d\n", my_strlen(arr1));
+
+	return 0; 
+}
 
 //
 //int main()
@@ -57,3 +82,16 @@
 //	printf("%d\n", num);
 //	return 0;
 //}
+
+
+//常见错误
+
+//1.语法错误
+
+//2.链接错误
+//要不这个符号不存在要不这个符号写错了
+//extern用于声明外部符号
+
+//3.运行错误
+//借助调试，逐步定位问题，最难
+
