fa85b17494b32668b81452c623d42f5df07a635e
Grand-breakeer <110274520+Grand-breakeer@users.noreply.github.com>
2022-09-11 17:15:59 +0800

    Add files via upload

--- 作业/20220909.c
+++ 作业/20220909.c
@@ -297,17 +297,140 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	int money = 0;
+//	scanf("%d", &money);
+//	int total = money;
+//	int empty = money;
+//	while (empty >= 2)
+//	{
+//		total += empty/2;
+//		empty = empty / 2+empty%2;
+//	}
+//	printf("%d\n", total);
+//	return 0;
+//}
+
+
+//给出一个数组调整元素顺序，奇数在前偶数在后
+
+//void print(int arr[],int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//
+//void move(int arr[], int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//
+//	//从前往后找偶数
+//	while (left < right)
+//	{
+//		while ((left<right)&&arr[left] % 2 == 1)//防止越界访问
+//		{
+//			left++;
+//		}
+//		//从后往前找奇数
+//		while (arr[right] % 2 == 0)
+//		{
+//			right--;
+//		}
+//		if (left < right)
+//		{
+//			int tmp = arr[left];
+//			arr[left] = arr[right];
+//			arr[right] = tmp;
+//		}
+//	}
+//}
+//
+//
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	move(arr, sz);
+//	print(arr, sz);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	unsigned char a = 200;
+//	//11001000
+//
+//	unsigned char b = 100;
+//	//01100100
+//	
+//	unsigned char c = 0;
+//	//A和B整型提升
+//	//00000000000000000000000011001000
+//	//00000000000000000000000001100100
+//	//00000000000000000000000100101100
+//
+//	c = a + b;
+//	//00101100
+//
+//	printf("%d %d", a + b, c);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int arr[10][10] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		for (j = 0; j <= i; j++)
+//		{
+//			if (j == 0)
+//			{
+//				arr[i][j] = 1;
+//			}
+//			if (i == j)
+//			{
+//				arr[i][j] = 1;
+//			}
+//			if (i >= 2 && j >= 1)
+//			{
+//				arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+//			}
+//		}
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//
+//
+//	return 0;
+//}
+
+
 int main()
 {
-	int money = 0;
-	scanf("%d", &money);
-	int total = money;
-	int empty = money;
-	while (empty >= 2)
+	char killer = 0;
+	for (killer = 'A'; killer <= 'D'; killer++)
 	{
-		total += empty/2;
-		empty = empty / 2;
+		if ((killer != 'A') + (killer == 'C') + (killer == 'D') + (killer != 'D') == 3)
+		{
+			printf("%c\n",killer);
+		}
 	}
-	printf("%d\n", total);
 	return 0;
 }
