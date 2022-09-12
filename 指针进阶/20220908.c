0a3c4201bf9ae792b102e5b3e69025d3c1bdbe14
Grand-breakeer <110274520+Grand-breakeer@users.noreply.github.com>
2022-09-12 12:58:28 +0800

    Add files via upload

--- 指针进阶/20220908.c
+++ 指针进阶/20220908.c
@@ -239,3 +239,136 @@
 //	int arr[3][5] = { 0 };
 //	test(arr);
 //}
+
+//void print(int* ptr, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", *(ptr + i));
+//	}
+//}
+//
+//void test(char* p)
+//{
+//
+//}
+//
+//
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int* p = arr;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//p是一级指针
+//	print(p, sz);//一级指针传参用一级指针接收
+//
+//	char ch = 'w';
+//	char* p1 = &ch;
+//	test(&ch);//char*
+//	test(p1);//同类型指针
+//	return 0;
+//}
+
+//void test(int** p2)
+//{
+//	**p2 = 20;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;//pa是一级指针
+//	int** ppa = &pa;//ppa是二级指针
+//	//把二级指针进行传参
+//
+//	test(ppa);
+//
+//	test(&pa);//传一级指针变量的地址
+//	int* arr[10] = { 0 };
+//	test(arr);//传存放一级指针的数组
+//	printf("%d ", a);
+//
+//	return 0;
+//}
+
+/*一级指针
+* int* p--整型指针--指向整型的指针
+* char* pc--字符指针--指向字符的指针
+* void* pv--无类型的指针
+* 
+* 
+* 二级指针
+* char** p
+* int** p
+* 
+* 数组指针：指向数组的指针
+* int(*p)[4];
+*/
+
+/*数组
+* 一维数组
+* 二维数组
+* 
+* 指针数组--存放指针的数组
+*/
+
+
+/*函数指针
+*指向函数的指针
+*/
+
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;
+//
+//	char ch = 'w';
+//	char* pc = &ch;
+//
+//	int arr[10] = { 0 };
+//	int (*parr)[10] = &arr;//取出数组的地址
+//	//parr是指向数组的指针-存放的是数组的地址
+//
+//	//函数指针--存放函数的指针
+//	//&函数名-取到的是函数的地址
+//
+//	int(*pf)(int, int) = &Add;//pf就是函数指针的变量
+//
+//	printf("%p\n", &Add);
+//	printf("%d\n", Add);
+//
+//	return 0;
+//}
+
+
+//void test(char* str)
+//{
+//
+//}
+//
+//int main()
+//{
+//	void (*pt)(char*) = &test;
+//}
+
+int Add(int x, int y)
+{
+	return x + y;
+}
+
+int main()
+{
+	//int(*pf)(int, int) = &Add;//pf就是函数指针的变量
+	int(*pf)(int, int) = Add;
+	//int ret=(*pf)(3, 5);//运用函数指针调用其所指向的函数
+	int ret = pf(3, 5);
+	printf("%d\n", ret);
+
+	return 0;
+}
