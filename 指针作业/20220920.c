e0304aa91083a86130e398ae655bd4d7722a185e
Grand-breakeer <110274520+Grand-breakeer@users.noreply.github.com>
2022-09-21 23:06:11 +0800

    Add files via upload

--- 指针作业/20220920.c
+++ 指针作业/20220920.c
@@ -67,45 +67,138 @@ p=malloc
 //	return 0;
 //}
 
-int find_num(int arr[3][3], int* r, int* c, int k)
-{
-	int x = 0;
-	int y = *c - 1;
-	while (x < *r && y >= 0)
-	{
-		if (arr[x][y] < k)
-		{
-			x++;
-		}
-		else if(arr[x][y]>k)
-		{
-			y--;
-		}
-		else
-		{
-			*r = x + 1;
-			*c = y + 1;
-			return 1;
-		}
-	}
-	return 0;
-}
+//int find_num(int arr[3][3], int* r, int* c, int k)
+//{
+//	int x = 0;
+//	int y = *c - 1;
+//	while (x < *r && y >= 0)
+//	{
+//		if (arr[x][y] < k)
+//		{
+//			x++;
+//		}
+//		else if(arr[x][y]>k)
+//		{
+//			y--;
+//		}
+//		else
+//		{
+//			*r = x + 1;
+//			*c = y + 1;
+//			return 1;
+//		}
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int arr[3][3] = { 1,2,3,4,5,6,7,8,9 };
+//	int k = 7;
+//	int r = 3;
+//	int c = 3;
+//	int ret = find_num(arr, &r, &c, k);
+//	if (ret == 1)
+//	{
+//		printf("找到了!!!\n");
+//		printf("元素在第%d行%d列\n", r, c);
+//	}
+//	else
+//	{
+//		printf("找不到!!!\n");
+//	}
+//	return 0;
+//}
 
-int main()
-{
-	int arr[3][3] = { 1,2,3,4,5,6,7,8,9 };
-	int k = 7;
-	int r = 3;
-	int c = 3;
-	int ret = find_num(arr, &r, &c, k);
-	if (ret == 1)
-	{
-		printf("找到了!!!\n");
-		printf("元素在第%d行%d列\n", r, c);
-	}
-	else
-	{
-		printf("找不到!!!\n");
-	}
-	return 0;
-}
+//依次转换思想
+//void string_left_rotate(char* str, int k)
+//{
+//	int i = 0;
+//	int len = strlen(str);
+//	for (i = 0; i < k; i++)
+//	{
+//		char tmp = *str;
+//
+//		int j = 0;
+//		for (j = 0; j < len - 1; j++)
+//		{
+//			*(str + j) = *(str + j + 1);
+//		}
+//		*(str + len - 1) = tmp;
+//	}
+//}
+//int main()
+//{
+//	char arr[10] = "ABCDEF";
+//	int k = 2;
+//	string_left_rotate(arr, k);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//三次逆序思想
+//void revers(char* left, char* right)
+//{
+//	assert(left);
+//	assert(right);
+//	while (left<right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//void string_left_rotate(char* str, int k)
+//{
+//	revers(str,str+k-1);
+//	revers(str + k, str + strlen(str) - 1);
+//	revers(str, str + strlen(str) - 1);
+//}
+//int main()
+//{
+//	char arr[10] = "ABCDEF";
+//	int k = 2;
+//	string_left_rotate(arr, k);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//int is_string_rotate(char* str1, char* str2)
+//{
+//	int i = 0;
+//	int n = strlen(str1);
+//	for (i = 0; i < n; i++)
+//	{
+//		char tmp = *str1;
+//		int j = 0;
+//		for (j = 0; j < n - 1; j++)
+//		{
+//			*(str1 + j) = *(str1 + j + 1);
+//		}
+//		*(str1 + n - 1) = tmp;
+//
+//		if (strcmp(str1, str2) == 0)
+//		{
+//			return 1;
+//		}
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	char arr1[] = "AABCD";
+//	char arr2[] = "BCDAA";
+//	int ret = is_string_rotate(arr1, arr2);
+//	if (ret == 1)
+//	{
+//		printf("yes\n");
+//	}
+//	else
+//	{
+//		printf("no\n");
+//	}
+//	return 0;
+//}
