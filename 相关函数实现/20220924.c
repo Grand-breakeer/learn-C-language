d94c6f008f6172f933ef8db56b3afa2570571c3d
Grand-breakeer <110274520+Grand-breakeer@users.noreply.github.com>
2022-09-25 21:30:11 +0800

    Add files via upload

--- 相关函数实现/20220924.c
+++ 相关函数实现/20220924.c
@@ -100,29 +100,179 @@
 
 //模拟实现strcat
 
-char* my_strcat(char* dest, char* src)
-{
-	//1.找到dest的\0
-	assert(dest && src);
-	int* tmp = dest;//保留初始地址
-	while (*dest)
-	{
-		dest++;
-	}
-	while (*dest++ = *src++)
-	{
-		;
-	}
-	return tmp;
-}
+//char* my_strcat(char* dest, char* src)
+//{
+//	//1.找到dest的\0
+//	assert(dest && src);
+//	int* tmp = dest;//保留初始地址
+//	while (*dest)
+//	{
+//		dest++;
+//	}
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return tmp;
+//}
+//
+//int main()
+//{
+//	char arr1[20] = "hello ";
+//	char arr2[] = "world";
+//	
+//
+//	printf("%s\n", my_strcat(arr1, arr2));
+//
+//	return 0;
+//}
 
-int main()
-{
-	char arr1[20] = "hello ";
-	char arr2[] = "world";
-	
+//int main()
+//{
+//	char arr[20] = "abcd";
+//	strcat(arr, arr);//找不到\0因为被覆盖了
+//	printf("%s\n", arr);
+//
+//	return 0;
+//}
 
-	printf("%s\n", my_strcat(arr1, arr2));
 
-	return 0;
-}
+//strcmp比较两个字符串的大小
+//int main()
+//{
+//	char* p = "obc";
+//	char* q = "abcdef";
+//	if (p > q)
+//	{
+//		printf(">\n");
+//	}
+//	else
+//	{
+//		printf("<=\n");
+//	}
+//
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	char* p = "abcdef";//大于返回1小于返回-1等于返回0
+//	char* q = "abcdef";
+//	if (strcmp(p,q))
+//	{
+//		printf(">\n");
+//	}
+//	else if(strcmp(p,q)<0)
+//	{
+//		printf("<\n");
+//	}
+//	else
+//	{
+//		printf("=\n");
+//	}
+//
+//	return 0;
+//}
+
+//int my_strcmp(const char* s1, const char* s2)
+//{
+//	assert(s1 && s2);
+//	while (*s1 || *s2)//当两个指针
+//	{
+//		if (*s1 > *s2)
+//		{
+//			return 1;
+//		}
+//		else if (*s1 < *s2)
+//		{
+//			return -1;
+//		}
+//		if (*s1 == '\0'&&*s2)
+//		{
+//			return 1;
+//		}
+//		else if (*s1 && *s2 == '\0')
+//		{
+//			return -1;
+//		}
+//		s1++;
+//		s2++;
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	char* p = "abcdef";
+//	char* q = "abcdef";
+//	int ret = my_strcmp(p, q);
+//
+//	if (ret > 0)
+//	{
+//		printf("p>q\n");
+//	}
+//	else if (ret < 0)
+//	{
+//		printf("p<q\n");
+//	}
+//	else
+//	{
+//		printf("p=q\n");
+//	}
+//	return 0;
+//}
+
+//strcpy-
+//strcat-
+//strcmp-
+//长度不受限制的字符串
+
+//strncpy
+//strncat
+//strncmp
+//长度受限制的字符串
+
+//strncpy
+
+//int main()
+//{
+//	char arr1[20] = "abcdef";
+//	char arr2[] = "qwer";
+//	strncpy(arr1, arr2, 2);//超出部分用0填充
+//	printf("%s\n", arr1);
+//
+//	return 0;
+//}
+
+//strncat
+//追加指定n个字符串
+
+//int main()
+//{
+//	char arr1[20] = "hello ";
+//	char arr2[] = "world";
+//
+//	strncat(arr1, arr2, 3);
+//
+//	printf("%s\n", arr1);
+//
+//	return 0;
+//}
+
+//strncmp
+//比较指定的n个字符
+
+//int main()
+//{
+//	char* p = "abcdef";
+//	char* q = "abcqwert";
+//	//int ret = strcmp(p, q);
+//
+//	int ret = strncmp(p, q, 3);
+//	printf("%d\n", ret);
+//
+//	return 0;
+//}
