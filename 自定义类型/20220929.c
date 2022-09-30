17936a92424993efac874774d854b5d8d3746f68
Grand-breakeer <110274520+Grand-breakeer@users.noreply.github.com>
2022-09-30 16:42:30 +0800

    Add files via upload

--- 自定义类型/20220929.c
+++ 自定义类型/20220929.c
@@ -122,3 +122,75 @@
 //	return 0;
 //}
 
+//结构体的内存对齐
+//结构体内存对齐规则：
+//结构体第一个成员在结构体变量偏移量为零的地址处
+//其他成员要对齐到某个数字（对齐数）的整数倍的地址处。
+//对齐数=编译器默认的一个对齐数与该成员大小的较小值
+//VS中默认为8  Linux平台无默认对齐数概念
+//结构体的总大小为结构体所有成员的最大对齐数的整数倍
+//如果嵌套了结构体，则嵌套结构体对齐到它最大对齐数的整数倍处
+//此时结构体的大小就是包含嵌套结构体内所有最大对齐数的整数倍
+
+struct S {
+	int i;
+	char c;
+};
+
+struct S2 {
+	char c1;
+	int i;
+	char c2;
+};
+
+struct S3 {
+	char c1;
+	int i;
+	double d;
+};
+
+struct S4 {
+	char c1;
+	char c2;
+	int i;
+};
+
+struct S5 {
+	int d;
+	char c;
+	int i;
+};
+
+struct S6 {
+	char c1;
+	struct S5 s5;
+	double d;
+};
+
+int main()
+{
+	struct S s = { 0 };
+	printf("%d\n", sizeof(s));//8
+
+	struct S2 s2 = { 0 };
+	printf("%d\n", sizeof(s2));//12
+
+	struct S3 s3 = { 0 };
+	printf("%d\n", sizeof(s3));//16
+
+	struct S4 s4 = { 0 };
+	printf("%d\n", sizeof(s4));//8
+
+	struct S5 s5;
+	printf("%d\n", sizeof(s5));//16
+
+	struct S6 s6;
+	printf("%d\n", sizeof(s6));//24
+	return 0;
+}
+
+//为何要存在内存对齐
+// 1.平台原因(移植原因)：不是所有的硬件平台都能访问任意地址上的任意数据的；
+// 某些硬件平台只能在某些地址处取某些特定类型的数据，否则抛出硬件异常。 
+//2.性能原因：数据结构(尤其是栈)应该尽可能地在自然边界上对齐。
+/*方便读取，所以拿空间换取时间*/
