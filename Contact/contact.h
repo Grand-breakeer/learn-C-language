60e743803494be2bc9233902ddd3cd999bbe50de
Grand-breakeer <110274520+Grand-breakeer@users.noreply.github.com>
2022-10-10 21:26:58 +0800

    Add files via upload

--- Contact/contact.h
+++ Contact/contact.h
@@ -5,20 +5,49 @@
 #define MAX_TELE 12
 #define MAX_ADDR 30
 #define MAX 1000
+#define DEFAULT_SZ 3
+#define INC_SZ 2
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 //类型的定义
 typedef struct PeoInfo 
 {
 	char name[MAX_NAME];
 	char sex[MAX_SEX];
 	int age;
-	char teke[MAX_TELE];
+	char tele[MAX_TELE];
 	char addr[MAX_ADDR];
 }PeoInfo;
 
-//通讯录
+//通讯录—静态版本
+//typedef struct Contact
+//{
+//	PeoInfo data[MAX];//存放添加进来的人的信息
+//	int sz;//记录当前通讯录中的有效信息个数
+//
+//}Contact;
+
+//动态版本
 typedef struct Contact
 {
-	PeoInfo data[MAX];//存放添加进来的人的信息
+	PeoInfo* data;//指向动态申请的空间用来存放联系人信息
 	int sz;//记录当前通讯录中的有效信息个数
-
+	int capacity;//记录当前通讯录容量
 }Contact;
+
+//初始化通讯录
+void InitContact(Contact* pc);
+//增加联系人
+void Addcontact(Contact* pc);
+//显示联系人信息
+void PrintContact(const Contact* pc);
+//删除联系人
+void DelContact(Contact* pc);
+//查找联系人
+void SearchContact(Contact* pc);
+//修改联系人
+void ModifyContact(Contact* pc);
+//销毁通讯录
+void DestoryContact(Contact* pc);
--- Contact/test.c
+++ Contact/test.c
@@ -2,7 +2,7 @@
 #include "contact.h"
 //test.c测试通讯录模块
 
-//通讯录
+//通讯录——静态版本
 //1.通讯录能够存放1000个人的信息
 // 每个人的信息：
 // 名字+年龄+性别+电话+地址
@@ -12,6 +12,11 @@
 // 5.查找指定人的信息
 // 6.排序通讯录的信息
 // 
+
+//版本2：动态增长的版本
+//1.通讯录初始化后，能够存放三个人的信息
+//2.当空间满了以后增加两个空间
+//
 
 
 void menu()
@@ -40,8 +45,12 @@ int main()
 {
 	int input = 0;
 	//创建通讯录
-	Contact con;
-	
+	Contact con;//通讯录
+	//初始化通讯录
+	//给data申请一片连续的空间在堆区上
+	//sz=0
+	//capacity初始化为最大容量
+	InitContact(&con);
 
 	do
 	{
@@ -51,19 +60,29 @@ int main()
 		switch (input)
 		{
 		case ADD:
+			Addcontact(&con);
 			break;
 		case DEL:
+			DelContact(&con);
 			break;
 		case SEARCH:
+			SearchContact(&con);
 			break;
 		case MODIFY:
+			ModifyContact(&con);
 			break;
 		case SORT:
 			break;
 		case PRINT:
+			PrintContact(&con);
 			break;
 		case EXIT:
+			//销毁通讯录
+			DestoryContact(&con);
+			printf("退出\n");
 			break;
+		default:
+			printf("选择错误\n");
 		}
 	} while (input);
 	return 0;
