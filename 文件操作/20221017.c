f25947c3743bae6f7f15918fbe7c81c03e826d73
Grand-breakeer <110274520+Grand-breakeer@users.noreply.github.com>
2022-10-18 17:03:32 +0800

    Add files via upload

--- 文件操作/20221017.c
+++ 文件操作/20221017.c
@@ -30,3 +30,120 @@
 //方便起见文件标识被称为文件名
 
 //文件的打开和关闭
+//缓冲文件系统中，关键的概念是“文件类型指针”，简称“文件指针”。
+//每个被使用的文件都在内存中开辟了一个相应的文件信息区，用来存放文件的相关信息（如文件的名
+//字，文件状态及文件当前的位置等）。这些信息是保存在一个结构体变量中的。该结构体类型是有系统
+//声明的，取名FILE
+
+
+/*struct _iobuf {
+    char* _ptr;
+    int   _cnt;
+    char* _base;
+    int   _flag;
+    int   _file;
+    int   _charbuf;
+    int   _bufsiz;
+    char* _tmpfname;
+};typedef struct _iobuf FILE;*///vs2013中文件信息区的定义
+
+/*不同的C编译器的FILE类型包含的内容不完全相同，但是大同小异。
+每当打开一个文件的时候，系统会根据文件的情况自动创建一个FILE结构的变量，并填充其中的信息，
+使用者不必关心细节。
+一般都是通过一个FILE的指针来维护这个FILE结构的变量，这样使用起来更加方便。
+下面我们可以创建一个FILE*的指针变量:*/
+
+//int main()
+//{
+//    FILE* pf = fopen("test.dat", "w");
+//    if (pf == NULL)
+//    {
+//        perror("fopen");
+//        return 1;
+//    }
+//    //写文件
+//
+//    //关闭文件
+//    fclose(pf);
+//    pf = NULL;
+//    return 0;
+//}
+
+//文件地址用双斜杠隔开，防止转义字符误识别
+
+//int main()
+//{
+//    FILE* pf = fopen("test.dat", "r");
+//    if (pf == NULL)
+//    {
+//        perror("fopen");
+//        return 1;
+//    }
+//    //写文件
+//    int ret = fgetc(pf);
+//    printf("%c\n", ret);
+//    ret = fgetc(pf);
+//    printf("%c\n", ret);
+//    ret = fgetc(pf);
+//    printf("%c\n", ret); 
+//    ret = fgetc(pf);
+//    printf("%c\n", ret);
+//
+//    fclose(pf);
+//    pf = NULL;
+//    return 0;
+//}
+
+//流：高度抽象的概念
+//程序会操作各种各样的硬件，每种硬件的读取方式都不同
+//所以程序向流里面输入数据，流再通过别的方式进行操作硬件
+
+//C语言程序只要运行起来就默认打开了三个流
+//stdin-标准输入流-键盘
+//stdout-标准输出流-屏幕
+//stderr-标准错误流-屏幕
+
+//int main()
+//{
+//    fputc('b', stdout);
+//    fputc('i', stdout);
+//    fputc('t', stdout);
+//    return 0;
+//}
+
+//int main()
+//{
+//    FILE* pf = fopen("test.dat", "w");
+//    if (pf == NULL)
+//    {
+//        perror(fopen);
+//        return 1;
+//    }
+//    //写文件--按行写
+//    fputs("abcdef\n", pf);
+//    fputs("qwertyuiop\n", pf);
+//
+//    fclose(pf);
+//    pf = NULL;
+//
+//    return 0;
+//}
+
+//int main()
+//{
+//    char arr[10] = { 0 };
+//    FILE* pf = fopen("test.dat", "r");
+//    if (pf == NULL)
+//    {
+//        perror(fopen);
+//        return 1;
+//    }
+//    //读文件--按行读
+//    fgets(arr,4,pf);
+//    printf("%s\n", arr);
+//    fgets(arr, 4, pf);
+//    printf("%s\n", arr);
+//    fclose(pf);
+//    pf = NULL;
+//    return 0;
+//}
